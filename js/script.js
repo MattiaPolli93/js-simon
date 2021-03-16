58241fbd07b87dd5a9d91482d08feff880c88d58
Mattia <matpolli93@gmail.com>
2021-03-16 14:29:34 +0100

    Added files

---
+++ js/script.js
@@ -0,0 +1,1 @@
+/* An alert() displays five randomly-generated numbers. Then a 30-second timer starts, after which the user must insert the previous numbers via prompt(). At the end, the computer displays how many and which numbers were guessed correctly  */
